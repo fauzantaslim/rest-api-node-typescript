68076958857a703ec291b53a6ed8896a57d1d5e9
fauzantaslim <fauzantaslim123@gmail.com>
2025-02-13 12:52:47 +0700

    fix eslint

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application, Request, Response } from 'express'
 
-const app: Application = express();
-const port: number = 3000;
-const host: string = 'localhost';
+const app: Application = express()
+const port: number = 3000
+const host: string = 'localhost'
 
-app.use('/', (req: Request, res: Response, next: NextFunction) => {
+app.use('/', (req: Request, res: Response) => {
   res.status(200).send({
-    message: 'welcome',
-  });
-});
+    message: 'welcome'
+  })
+})
 
 app.listen(port, host, () => {
-  console.log(`server running at http://${host}:${port}`);
-});
+  console.log(`server running at http://${host}:${port}`)
+})
