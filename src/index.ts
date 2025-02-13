4480ee72b07fc8641e7dc5dce9626fa74ed22176
Fauzan Taslim <fauzantaslim123@gmail.com>
2025-02-13 11:03:19 +0700

    Initialize Project

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+const app: Application = express();
+const port: number = 3000;
+const host: string = 'localhost';
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({
+    message: 'welcome',
+  });
+});
+
+app.listen(port, host, () => {
+  console.log(`server running at http://${host}:${port}`);
+});
